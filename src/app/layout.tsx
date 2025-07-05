51f5b92506e5780756bd367c98041ae4f8fd5ee5
Tom Keddie <thomaskeddie@gmail.com>
2025-07-05 17:07:01 +0000

    i did everything you said but still no favicon or black app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/trimology-logo.png" sizes="any" />
       </head>
       <body className="font-body antialiased">
         {children}
