bafc07fbfe8c1a2825ee86154ef35a0ba9302504
Tom Keddie <thomaskeddie@gmail.com>
2025-08-08 08:36:51 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  projectId: "trimology",
+  appId: "1:170094637668:web:73f711de3dd200ff11e571",
+  storageBucket: "trimology.appspot.com",
+  apiKey: "AIzaSyBUZq9DtKvHSeyWOWOQTp_D7g8i4OOYQMM",
+  authDomain: "trimology.firebaseapp.com",
+  messagingSenderId: "170094637668",
 };
 
 // Initialize Firebase
